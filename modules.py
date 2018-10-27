0c4ff8ef1eedce5dd47b01bdd935cbbfedf7fa30
Jake Choi <jake@Jakeui-MacBook-Pro.local>
2018-10-27 18:38:19 +0900

    first commit

--- modules.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-
-def getURL():
-	twitch_id = raw_input("Enter your twitch ID (example : pjkrveil) : ")
-	start_urls = 'https://www.twitch.tv/' + twitch_id
-
-	return start_urls
