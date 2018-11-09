d79aa0ce050aab63f5c49e0239a192efa5faf4f0
Jake Choi <jake@Jakeui-MacBook-Pro.local>
2018-11-09 17:39:59 +0900

    first commit

--- chat_cwl.py
+++ chat_cwl.py
@@ -13,3 +13,37 @@ def getURL():
 def parseData(start_urls)
 	with open(start_urls) as fp:
     	soup = BeautifulSoup(fp, 'html.parser')
+
+    	# get chatting logs
+    	username_info = soup.find_all("button", {"class":"chat-line__username"})
+
+    	# crawl list of user data
+    	l_user_display_name = username_info.find_all("span", {"class":"chat-author__display-name"})
+    	l_user_login_id = username_info.find_all("span", {"class":"chat-author__intl-login"})
+    	l_user_chat_text = username_info.find_all("span", {"class":"text-fragment"})
+
+    	# list for saving user data
+    	user_display_name = []
+    	user_login_id = []
+    	user_id_color = []
+
+    	for user_dp_name in l_user_display_name:
+    		# extract user_display_name text data
+    		user_display_name.append(user_dp_name.get_text())
+
+    		# extract color values
+    		color_val = (user_dp_name.split("(")[-1])[:-1]
+    		user_id_color.append(color_val)
+
+    	for user_login_id in l_user_login_id:
+    		# extract user_login_id text data
+    		user_login_id.append(user_login_id.get_text())
+
+    	
+
+
+
+
+
+
+
